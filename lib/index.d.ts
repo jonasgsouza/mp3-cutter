de41395542947053186e5db70bd0a0ba030568e4
jonasgsouza <jonas.gdsouza@gmail.com>
2023-07-27 05:04:33 +0000

    type def

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,5 @@
-import { Duplex } from "./cutter";
-
 declare module 'mp3-cutter' {
-    declare class MP3Cutter extends Duplex {
+    declare class MP3Cutter {
 
         constructor(options: { input: Buffer | string, start: number, end: number });
 
