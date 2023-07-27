5dd94846d7a1893ea6c2f187509d5bfbefc5e5cb
jonasgsouza <jonas.gdsouza@gmail.com>
2023-07-27 05:02:23 +0000

    type def

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,10 @@
-declare class MP3Cutter {
-
-    constructor(options: { input: Buffer | string, start: number, end: number });
-
-    toFile(dest: string): void;
-
-    toBuffer(): Buffer;
-}
-
 declare module 'mp3-cutter' {
-    export = MP3Cutter
+    declare class MP3Cutter {
+
+        constructor(options: { input: Buffer | string, start: number, end: number });
+
+        toFile(dest: string): void;
+
+        toBuffer(): Buffer;
+    }
 }
